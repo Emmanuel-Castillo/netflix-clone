6e8efdcbe1029b8a838a3e35560f4db77c790d8c
Emmanuel Castillo <emmanuel.2001alonso.ec@gmail.com>
2023-07-19 12:00:54 -0500

    added rows and thumbnails

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='relative h-screen lg:h-[140vh]'>
       <Head />
       <body>
         <Main />
